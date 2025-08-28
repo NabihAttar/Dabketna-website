82abb4c6d51b9ceec332a996002e46c471915b0d
Nabih Attar <nabihattarr@gmail.com>
2025-08-28 17:31:24 +0300

    last version

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
   experimental: {
-    // prevents Next.js from collecting traces, avoiding stack overflow
-    scrollRestoration: false,
-    craCompat: false,
-    outputFileTracingExcludes: {
-      '**/node_modules/**': true,
-      '**/.next/**': true,
-      '**/public/**': true,
-    },
+    outputFileTracingExcludes: [
+      '**/node_modules/**',
+      '**/.next/**',
+      '**/public/**'
+    ],
   },
 };
 
