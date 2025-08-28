1ce26c20dc90194f94c70e6cb5312d013f731d2c
Nabih Attar <nabihattarr@gmail.com>
2025-08-28 17:08:14 +0300

    Remove React Native async-storage

--- next.config.js
+++ next.config.js
@@ -1,23 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
+  },
+  experimental: {
+    // prevents Next.js from collecting traces, avoiding stack overflow
+    scrollRestoration: false,
+    craCompat: false,
   },
 };
 
