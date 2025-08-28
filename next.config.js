4c5d5fc83017b5ff3b2fe6ee82410511012ea296
Nabih Attar <nabihattarr@gmail.com>
2025-08-28 16:53:54 +0300

    Fix WorkArea for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'], // only scan these files
+  reactStrictMode: true, // recommended
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // allow all remote images
+      },
+    ],
+  },
+  webpack(config) {
+    // Add support for importing SVGs as React components
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
