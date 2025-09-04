74bd0f7925f0860fa280f9d8c0961131975853d2
Nabih Attar <nabihattarr@gmail.com>
2025-09-04 14:20:46 +0300

    fix: export default Page

--- src/components/genres/GenresListingArea.tsx
+++ src/components/genres/GenresListingArea.tsx
@@ -9,26 +9,11 @@ import React from "react";
 import PaginationData from "../common/pagination/pagination-data";
 import NiceSelect from "../common/NiceSelect";
 
+// Types
 type TNiceSelectData = {
   id: number;
   optionName: string;
-};
-
-type TGenresListing = {
-  id: number;
-  title: string;
-  image: string;
-  price: number;
-  videoUrl?: string;
-  activeClass?: string;
-  description?: string;
-  mapLink?: string;
-  location?: string;
-  ratings?: number;
-  ratingNum?: number;
-  duration?: string;
-  people?: string;
-  smallImages?: string[]; // optional property
+  smallImages?: string[];
 };
 
 const GenresListingArea = () => {
@@ -118,7 +103,7 @@ const GenresListingArea = () => {
             tabIndex={0}
           >
             <div className="row">
-              {genres_listing_data.slice(0, 14).map((item: TGenresListing) => (
+              {genres_listing_data.slice(0, 14).map((item) => (
                 <div className="col-xl-6" key={item.id}>
                   <div className="ms-genres-item ms-genres-flex mb-25">
                     <div className="ms-genres-img ms-br-15 fix w-img genres-img-214">
@@ -132,6 +117,7 @@ const GenresListingArea = () => {
                           alt="genres img"
                         />
                       </Link>
+
                       {item.videoUrl && (
                         <Link
                           className="popup-video ms-genres-video"
@@ -140,8 +126,12 @@ const GenresListingArea = () => {
                           <i className="fa-sharp fa-solid fa-play"></i>
                         </Link>
                       )}
-                      <span className="ms-genres-price">From ${item.price}</span>
+
+                      <span className="ms-genres-price">
+                        From ${item.price}
+                      </span>
                     </div>
+
                     <div className="ms-genres-content p-relative">
                       <span className={`ms-genres-star ${item.activeClass}`}>
                         <i className="fa-light fa-star"></i>
@@ -152,11 +142,11 @@ const GenresListingArea = () => {
                         </Link>
                       </h4>
                       <p className="mb-30">{item.description}</p>
+
                       <div className="ms-fun-brand-bottom ms-genres-rating">
                         <div className="ms-fun-brand-location">
                           <Link href={item.mapLink} target="_blank">
-                            <i className="flaticon-pin"></i>
-                            {item.location}
+                            <i className="flaticon-pin"></i> {item.location}
                           </Link>
                         </div>
                         <div className="ms-fun-brand-rating">
@@ -169,6 +159,7 @@ const GenresListingArea = () => {
                 </div>
               ))}
             </div>
+
             <div className="row">
               <div className="col-xl-12">
                 <PaginationData />
@@ -185,7 +176,7 @@ const GenresListingArea = () => {
             tabIndex={0}
           >
             <div className="row">
-              {genres_listing_data.slice(14, 23).map((item: TGenresListing) => (
+              {genres_listing_data.slice(14, 23).map((item) => (
                 <div className="col-xl-4 col-lg-6" key={item.id}>
                   <div
                     style={{
@@ -237,13 +228,23 @@ const GenresListingArea = () => {
                         loader={imageLoader}
                         alt="package img"
                         unoptimized
-                        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
                       />
                     </div>
 
                     {/* Small Images */}
-                    <div style={{ display: "flex", gap: "8px", marginBottom: "16px" }}>
-                      {(item.smallImages?.length
+                    <div
+                      style={{
+                        display: "flex",
+                        gap: "8px",
+                        marginBottom: "16px",
+                      }}
+                    >
+                      {(item.smallImages && item.smallImages.length > 0
                         ? item.smallImages
                         : [item.image, item.image, item.image]
                       ).map((img: string, index: number) => (
@@ -262,7 +263,11 @@ const GenresListingArea = () => {
                             loader={imageLoader}
                             alt={`small-${index}`}
                             unoptimized
-                            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              objectFit: "cover",
+                            }}
                           />
                         </div>
                       ))}
@@ -281,11 +286,23 @@ const GenresListingArea = () => {
                         color: "#aaa",
                       }}
                     >
-                      <div style={{ display: "flex", alignItems: "center", gap: "6px" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "6px",
+                        }}
+                      >
                         <i className="fa-regular fa-clock"></i>
                         <span>{item.duration || "30 mins"}</span>
                       </div>
-                      <div style={{ display: "flex", alignItems: "center", gap: "6px" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "6px",
+                        }}
+                      >
                         <i className="fa-regular fa-user"></i>
                         <span>{item.people || "10"}</span>
                       </div>
@@ -300,8 +317,14 @@ const GenresListingArea = () => {
                         fontSize: "13px",
                       }}
                     >
-                      <li>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</li>
-                      <li>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</li>
+                      <li>
+                        Donec pede justo, fringilla vel, aliquet nec, vulputate
+                        eget, arcu.
+                      </li>
+                      <li>
+                        In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
+                        justo.
+                      </li>
                       <li>Nullam dictum felis eu pede mollis pretium.</li>
                     </ul>
 
@@ -342,6 +365,7 @@ const GenresListingArea = () => {
                 </div>
               ))}
             </div>
+
             <div className="row">
               <div className="col-xl-12">
                 <PaginationData />
