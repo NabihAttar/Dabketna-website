1555bd14fac3a6c72aeba4eb3c8a79106d6d7c09
Nabih Attar <nabihattarr@gmail.com>
2025-09-11 14:33:28 +0300

    update

--- src/components/event-details/EventDetailsMain.tsx
+++ src/components/event-details/EventDetailsMain.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import Breadcrumb from '../common/breadcrumb/Breadcrumb';
 import Image from 'next/image';
 import { imageLoader } from '@/hooks/ImageLoader';
-import chooseBgImg from '../../../public/assets/img/choose/choose-bg.png'
+import chooseBgImg from '../../../public/assets/img/choose/choose-bg.png';
 import { TLetestNews, idType } from '@/types/types';
 import latest_news_data from '@/data/latest-news-data';
 import Link from 'next/link';
 import EventDetailsSidebar from '../event-details/EventDetailsSidebar';
 
 const EventDetailsMain = ({ id }: idType) => {
-    const event: TLetestNews | undefined = latest_news_data.find((item) => item.id == id);
+    const event: TLetestNews | undefined = latest_news_data.find((item) => item.id === id);
 
     return (
         <>
@@ -39,20 +39,20 @@ const EventDetailsMain = ({ id }: idType) => {
                             <div className="ms-event-details-content">
                                 <h2 className="ms-event-dtitle mb-20">{event?.title}</h2>
 
-                                 <p className="mb-25">
-                                    Grursus mal suada faci lisis Lorem ipsum dolarorit more a ametion consectetur elit. Vesti at bulum nec odio aea the dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit to the consectetur elit. All Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.
+                                <p className="mb-25">
+                                    Our hand-picked acts will guarantee you fantastic wedding entertainment for each part. We&apos;ll provide help and support 24 hours a day, 7 days a week...
                                 </p>
 
-                                 <p className="mb-25">
-                                    Grursus mal suada faci lisis Lorem ipsum dolarorit more a ametion consectetur elit. Vesti at bulum nec odio aea the dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit to the consectetur elit. All Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.
+                                <p className="mb-25">
+                                    Alienum phaedrum torquatos nec eu, vis detraxit periculis ex, nihil expetendis in mei...
                                 </p>
 
                                 <p className="mb-25">
                                     Grursus mal suada faci lisis Lorem ipsum dolarorit more a ametion consectetur elit. Vesti at bulum nec odio aea the dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit to the consectetur elit. All Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.
                                 </p>
 
-                                 <p className="mb-25">
-                                    Grursus mal suada faci lisis Lorem ipsum dolarorit more a ametion consectetur elit. Vesti at bulum nec odio aea the dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit to the consectetur elit. All Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.
+                                <p className="mb-25">
+                                    Our hand-picked acts will guarantee you fantastic wedding entertainment for each part. We&apos;ll provide help and support 24 hours a day, 7 days a week, right up until Outstanding wedding band playing chart, indie, rock anthems and Britpop and featuring guitars, keyboards.
                                 </p>
                             </div>
 
